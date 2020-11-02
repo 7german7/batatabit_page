12b78645ac874cbb71726c5bec81458629c72f95
7german7 <german.junior93@gmail.com>
2020-11-02 00:40:11 -0400

    update

--- js/functions.js
+++ js/functions.js
@@ -21,25 +21,35 @@ function hideMenu() {
   button.classList.add('showButton');
 }
 
-
-var tablet = window.matchMedia("screen and (min-width: 727px)");
-tablet.addListener(validation);
-function validation(event) {
-  console.log(event.matches);
-}
-
-if(tablet.matches){
-  /*Mostrar menu*/
-  var menu = document.getElementById('nav-menu__container');
-  menu.classList.remove('hideMenu');
-  menu.classList.add('showMenu');
-
+function hideElements(){
   /*Ocultar boton*/
-  var button = document.getElementById('button-menu__container');
-  button.classList.remove('showButton');
-  button.classList.add('hideButton');
+   var button = document.getElementById('button-menu__container');
+   button.classList.remove('showButton');
+   button.classList.add('hideButton');
 
-  /*Ocultar boton X*/
-  var list_button = document.getElementById('hideMenu');
-  list_button.classList.add('hide__list-item');
+//   /*Ocultar boton X*/
+   var list_button = document.getElementById('hideMenu');
+   list_button.classList.add('hide__list-item');
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  if(screen.width<=726){
+    hideMenu();
+  }
+  if(screen.width>=727){
+    hideElements();
+  }
+});
+
+document.addEventListener('scroll',function(){
+  var menu = document.getElementById('nav-menu__container');
+   
+  if (window.scrollY>0) {
+    menu.classList.add('fixed-top');
+    menu.style.position = 'fixed';
+  }
+  else{
+    menu.classList.remove('fixed-top');
+    menu.removeAttribute('style');
+  }
+});
